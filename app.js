e72b465395d0c0badc1e2761a28dce4beca45909
Ludmilla Victória Dellagnesse <87335554+ludhdellagnesse@users.noreply.github.com>
2021-09-29 19:16:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function calculoMedia()
+{
+  var media;
+  var name = parseInt(document.getElementById('n1').value);
+  var n1 = parseInt(document.getElementById('n1').value);
+  var n2 = parseInt(document.getElementById('n2').value);
+  var n3 = parseInt(document.getElementById('n3').value);
+  var n4 = parseInt(document.getElementById('n4').value);
+  media = (n1 + n2 + n3 + n4) / 4;
+  
+}
+document.write(media)
